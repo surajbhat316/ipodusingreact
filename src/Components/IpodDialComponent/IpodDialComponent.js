c4e9e351cc6567ed79f2ce7d7d4fad61b745b9a0
Avineet Bhat <surajbhat316@gmail.com>
2023-09-23 09:51:24 +0530

    rotate functionality working fine

--- src/Components/IpodDialComponent/IpodDialComponent.js
+++ src/Components/IpodDialComponent/IpodDialComponent.js
@@ -5,17 +5,17 @@ import ZingTouch from 'zingtouch';
 export default function IpodDialComponent(props) {
     const {printAngle} = props;
     useEffect(()=>{ 
-        var target = document.body;
-        var region = new ZingTouch.Region(target);
-        region.bind(target, 'rotate', function(e) {
-            // console.log(e.detail.angle+ "Rotate");
-            printAngle(e.detail.angle);
-        }, []);
-        
+        // var target = document.body;
+        // var region = new ZingTouch.Region(target);
+        // region.bind(target, 'rotate', function(e) {
+        //     // console.log(e.detail.angle+ "Rotate");
+        //     printAngle(e.detail.angle);
+        // }, []);
+
     });
   return (
     <div>
-         <button id='menuBtn'>
+         {/* <button id='menuBtn'>
             Menu
         </button>
         <button id='forwardBtn'>
@@ -26,10 +26,12 @@ export default function IpodDialComponent(props) {
         </button>
         <button id='backwardBtn'>
             Back
-        </button>
+        </button> */}
         <div id='btnContainer'>
         </div>
     </div>
+
+    
     
   )
 }
--- src/Components/IpodMainComponent/IpodMainComponent.js
+++ src/Components/IpodMainComponent/IpodMainComponent.js
@@ -1,8 +1,9 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import './IpodMainComponent.css';
 import IpodScreenComponent from '../IpodScreenComponent/IpodScreenComponent';
 import IpodMenuComponent from '../IpodMenuComponent/IpodMenuComponent';
 import IpodDialComponent from '../IpodDialComponent/IpodDialComponent';
+import ZingTouch from "zingtouch";
 
 function reducer(state, action) {
   if (action.type === 'increment') {
@@ -34,24 +35,63 @@ export default function IpodMainComponent() {
 
 
   const printAngle = (angle) =>{
-    if(angle >= state.rotationAngle){
-      dispatch({type: "increment", payload: {flag: true, angle}});
+    if(angle >=0 && angle <= 90){
+      console.log("Songs");
     }
-    else{
-      dispatch({type: "increment", payload: {flag: false, angle}});
+    else if(angle >=91 && angle <= 180){
+      console.log("Albums");
     }
-    // console.log(state);
+    else if(angle >=181 && angle <= 270){
+      console.log("Artists");
+    }
+    else if(angle >=271 && angle <= 360){
+      console.log("Playlists");
+    }
+    // if(angle >= state.rotationAngle){
+    //   dispatch({type: "increment", payload: {flag: true, angle}});
+    // }
+    // else{
+    //   dispatch({type: "increment", payload: {flag: false, angle}});
+    // }
   }
+
+
+  useEffect(()=>{
+    var currentAngle = 15;
+    //Using a layer on top of the entire page for "fat-finger" detection on mobile devices.
+    document.getElementById('rotatable').style.transform = 'rotate(15deg)';
+
+    var target = document.getElementById('interaction');
+    var region = new ZingTouch.Region(target);
+
+    region.bind(target, 'rotate', function(e) {
+      var rotatable = document.getElementById('rotatable');
+      currentAngle += e.detail.distanceFromLast;
+      rotatable.style.transform = 'rotate(' + currentAngle + 'deg)';
+      printAngle(Math.floor(e.detail.angle));
+    });
+  });
   return (
-    <div id='main'>
-        <div id='menuAndScreen'>
-            <IpodMenuComponent flag={state.flag} />
-            <IpodScreenComponent />
+    <div>
+        <div id='main'>
+            <div id='menuAndScreen'>
+                <IpodMenuComponent flag={state.flag} />
+                <IpodScreenComponent />
+            </div>
+            <div id='dialComponent'>
+                <IpodDialComponent printAngle={printAngle} />
+                <div id="wrapper">
+                  <div id="rotate-container">
+                    <div id="rotatable">
+                      <div>Rotate me</div>
+                    </div>
+                  </div>
+                  <div id="output"></div>
+                </div>
+                <div style={{marginLeft: "-400px"}} id="interaction"></div>
+            </div>
         </div>
-        <div id='dialComponent'>
-            <IpodDialComponent printAngle={printAngle} />
-        </div>
-        
     </div>
+
   )
 }
